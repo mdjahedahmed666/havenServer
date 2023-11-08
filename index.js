fdd524333c1bb33b1722d59098eedd8cdc96d0d2
Jahed <mdjahedahmed666@gmail.com>
2023-11-08 09:52:19 +0000

    booking delete api is added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -71,6 +71,14 @@ async function run() {
       const result = await roomCollection.updateOne(query, room, options);
       res.send(result);
     });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
